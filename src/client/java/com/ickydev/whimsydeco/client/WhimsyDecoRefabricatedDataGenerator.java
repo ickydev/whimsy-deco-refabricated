b61b19015bd8c93fe158feb031e1ba71725ba5b6
ickyDev <67420833+ickydev@users.noreply.github.com>
2026-05-27 13:51:41 -0300

    Initial commit: Fabric template setup

---
+++ src/client/java/com/ickydev/whimsydeco/client/WhimsyDecoRefabricatedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ickydev.whimsydeco.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WhimsyDecoRefabricatedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
